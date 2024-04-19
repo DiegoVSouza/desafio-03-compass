03978cf2ef470629a4327e373e2d9821fb8bddd0
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-18 22:23:56 -0300

    feat: features component

---
+++ src/Presentation/Components/Features/Features.tsx
@@ -0,0 +1,43 @@
+import { Text, Flex, Image, Box } from '@chakra-ui/react';
+import './Features.css';
+import trophy from '../../assets/images/trophy 1.svg'
+import guarantee from '../../assets/images/guarantee.svg'
+import shipping from '../../assets/images/shipping.svg'
+import customerSupport from '../../assets/images/customer-support.svg'
+
+export default function Features() {
+
+    return (
+        <Flex id='features' flexWrap='wrap' mt='4.25rem' gap={['2rem','2rem','2rem','2rem','0']} height={['100%','100%','100%','100%','17rem']} as='section'
+         alignItems='center' justifyContent='space-between'  padding={['2rem','2rem 2.5rem','2rem 2.5rem','2rem 3.3rem','0 3.3rem']}>
+            <Flex>
+                <Image src={trophy} />
+                <Box>
+                    <Text fontWeight='bold'>High Quality</Text>
+                    <Text  >crafted from top materials</Text>
+                </Box>
+            </Flex>
+            <Flex>
+                <Image src={guarantee} />
+                <Box>
+                    <Text fontWeight='bold'>Warranty Protection</Text>
+                    <Text  >Over 2 years</Text>
+                </Box>
+            </Flex>
+            <Flex>
+                <Image src={shipping} />
+                <Box>
+                    <Text fontWeight='bold'>Free Shipping</Text>
+                    <Text  >Order over 150 $</Text>
+                </Box>
+            </Flex>
+            <Flex>
+                <Image src={customerSupport} />
+                <Box>
+                    <Text fontWeight='bold'>24 / 7 Support</Text>
+                    <Text  >Dedicated support</Text>
+                </Box>
+            </Flex>
+        </Flex>
+    );
+}
