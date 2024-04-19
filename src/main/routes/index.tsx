7b395ecb68322c08909bbdc7329f875f3d937bc4
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-18 22:25:03 -0300

    fix: scroll error on change pages

--- src/main/routes/index.tsx
+++ src/main/routes/index.tsx
@@ -1,17 +1,14 @@
 // Routes
-import { useSelector } from "react-redux";
 import Footer from "../../Presentation/Components/Footer/Footer";
 import AppRoutes from "./app.routes";
-import { useEffect, useRef } from 'react';
-import { UserStore } from "../../Domain/Model/Token";
 import Header from "../../Presentation/Components/Header/Header";
+import ScrollToTopOnNavigate from "../../utils/ScrollToTopOnNavigate";
 
 export default function Routes() {
-    const { user } = useSelector((store: UserStore) => store);
-    
+
     return (
         <>
-
+            <ScrollToTopOnNavigate />
             <Header />
             <AppRoutes />
             <Footer />
