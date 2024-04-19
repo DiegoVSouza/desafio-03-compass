4b439d3086eacb8566506653a8dc8ddc647e8717
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-18 22:26:30 -0300

    update: loader

--- src/Presentation/Components/Notification/LoadingSpinner.tsx
+++ src/Presentation/Components/Notification/LoadingSpinner.tsx
@@ -1,7 +1,9 @@
-import { Spinner } from "@chakra-ui/react";
+import { Box, Spinner } from "@chakra-ui/react";
 
 export default function LoadingSpinner(){
     return(
+        <Box w='100%' padding='5rem 0'>
         <Spinner w='3rem' height='3rem'/>
+        </Box>
     )
 }
