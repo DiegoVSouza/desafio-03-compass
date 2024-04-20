7fad4d69ea21f0238375e67ff4c82c75ea50f960
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-19 22:28:47 -0300

    update: routes

--- src/main/routes/app.routes.tsx
+++ src/main/routes/app.routes.tsx
@@ -5,6 +5,7 @@ import LoginPage from '../../Presentation/Pages/Login/LoginView';
 import UserPage from '../../Presentation/Pages/UserPage/UserPage';
 import ShopView from '../../Presentation/Pages/Shop/ShopView';
 import ProductView from '../../Presentation/Pages/Product/ProductView';
+import ShopCarView from '../../Presentation/Pages/ShopCar/ShopCarView';
 
 const Router: React.FC = ()=> {
   return (
@@ -12,7 +13,8 @@ const Router: React.FC = ()=> {
       <Route path='/home/' element={<HomeView/>}/>
       <Route path='/home/shop/' element={<ShopView/>}/>
       <Route path='/home/shop/:productname' element={<ProductView />} />
-      <Route path='/stores/' element={<UserPage/>}/>
+      <Route path='/home/shop-car/' element={<ShopCarView/>}/>
+      <Route path='/userpage/' element={<UserPage/>}/>
       <Route path='/login/' element={<LoginPage/>}/>
     </Routes>
   );
