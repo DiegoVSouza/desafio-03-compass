2f9fcf2665e88ee4d7504125f12d608a344b38ac
Diego Vieira <dvsdiegovieira@gmail.com>
2024-04-25 08:59:52 -0300

    update: make sure is a number

--- src/utils/FormatCurrency.ts
+++ src/utils/FormatCurrency.ts
@@ -1,5 +1,5 @@
 export default function formatCurrency(value:number) {
-  return value.toLocaleString('pt-BR', {
+  return Number(value).toLocaleString('pt-BR', {
     style: 'currency',
     currency: 'BRL',
     minimumFractionDigits: 2,
